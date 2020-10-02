706cd6f7de30bd26ab2655b880c19f1e030579e8
Max Gerringer <max.gerringer@gmail.com>
2020-10-01 20:00:21 -0400

    troubeshoot login/register

--- models/user.js
+++ models/user.js
@@ -25,6 +25,10 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     isAdmin: {
       type: DataTypes.BOOLEAN,
       defaultValue: false
